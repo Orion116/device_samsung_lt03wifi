937e77f8721d3d436ce21a452866488e350c69f6
Josh <joshwiley115@gmail.com>
2015-06-29 21:44:18 -0500

    Keep pesky users from modifing ROM zips

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -186,5 +186,8 @@ WIFI_DRIVER_FW_PATH_PARAM        := "/sys/module/dhd/parameters/firmware_path"
 WIFI_DRIVER_FW_PATH_STA          := "/system/etc/wifi/bcmdhd_sta.bin"
 WIFI_DRIVER_FW_PATH_AP           := "/system/etc/wifi/bcmdhd_apsta.bin"
 
+# Keepin it block based for the sake of SuperSu
+BLOCK_BASED_OTA=true
+
 # inherit from the proprietary version
 -include vendor/samsung/lt03wifi/BoardConfigVendor.mk
