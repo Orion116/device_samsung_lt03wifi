5e68dec8a2672c9c9ac3cf991b861df09adf5b15
Sean Hoyt <seanhoyt963@gmail.com>
2015-07-14 20:06:20 -0500

    Move to lt03-common

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -14,26 +14,10 @@
 # limitations under the License.
 #
 
+# Inherit from lt03-Common
+-include device/samsung/lt03-Common/BoardConfigCommon.mk
+
 LOCAL_PATH := device/samsung/lt03wifi
-
-# Platform
-BOARD_VENDOR := samsung
-TARGET_BOARD_PLATFORM := exynos5
-TARGET_SLSI_VARIANT := insignal
-TARGET_SOC := exynos5420
-
-# Architecture
-TARGET_ARCH := arm
-TARGET_ARCH_VARIANT := armv7-a-neon
-TARGET_CPU_ABI := armeabi-v7a
-TARGET_CPU_ABI2 := armeabi
-TARGET_CPU_SMP := true
-TARGET_CPU_VARIANT := cortex-a15
-
-# Bionic Tuning
-ARCH_ARM_USE_MEMCPY_ALIGNMENT := true
-BOARD_MEMCPY_ALIGNMENT := 64
-BOARD_MEMCPY_ALIGN_BOUND := 32768
 
 # Bluetooth
 BOARD_HAVE_BLUETOOTH := true
@@ -44,150 +28,8 @@ BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(LOCAL_PATH)/bluetooth
 # Bootloader
 TARGET_OTA_ASSERT_DEVICE := lt03wifi,lt03wifiue
 
-# Camera
-BOARD_NEEDS_MEMORYHEAPION := true
-COMMON_GLOBAL_CFLAGS += -DDISABLE_HW_ID_MATCH_CHECK
-COMMON_GLOBAL_CFLAGS += -DSAMSUNG_CAMERA_HARDWARE
-COMMON_GLOBAL_CFLAGS += -DSAMSUNG_DVFS
-
 # Kernel
-BOARD_KERNEL_BASE := 0x10000000
-BOARD_KERNEL_PAGESIZE := 2048
 TARGET_KERNEL_CONFIG := BrokenNote_lt03wifi_defconfig
-TARGET_KERNEL_SOURCE := kernel/samsung/lt03wifi
-KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/arm/arm-eabi-4.8/bin
-KERNEL_TOOLCHAIN_PREFIX := arm-eabi-
-
-# LIQUID OPTIMIZATIONS
-LIQUIFY=true
-
-# Charger/Healthd
-BOARD_CHARGER_ENABLE_SUSPEND := true
-CHARGING_ENABLED_PATH := "/sys/class/power_supply/battery/batt_lp_charging"
-
-# Bootloader
-TARGET_BOOTLOADER_BOARD_NAME := universal5420
-TARGET_NO_BOOTLOADER := true
-TARGET_NO_RADIOIMAGE := true
-
-# FIMG2D
-BOARD_USES_SKIA_FIMGAPI := true
-BOARD_USES_NEON_BLITANTIH := true
-
-# Graphics
-BOARD_EGL_CFG := $(LOCAL_PATH)/configs/egl.cfg
-OVERRIDE_RS_DRIVER := libRSDriverArm.so
-USE_OPENGL_RENDERER := true
-NUM_FRAMEBUFFER_SURFACE_BUFFERS := 5
-
-# HWCServices
-BOARD_USES_HWC_SERVICES := true
-
-# HDMI
-BOARD_USES_GSC_VIDEO := true
-BOARD_USES_CEC := true
-
-# GSC
-BOARD_USES_ONLY_GSC0_GSC1 := true
-
-# FIMG2D
-BOARD_USES_SKIA_FIMGAPI := true
-BOARD_USES_NEON_BLITANTIH := true
-
-# SCALER
-BOARD_USES_SCALER := true
-
-# Include path
-TARGET_SPECIFIC_HEADER_PATH := $(LOCAL_PATH)/include
-
-# Hardware
-BOARD_HARDWARE_CLASS += device/samsung/lt03wifi/cmhw
-
-# Init
-TARGET_NR_SVC_SUPP_GIDS := 20
-
-# OpenMAX Video
-BOARD_USE_STOREMETADATA := true
-BOARD_USE_METADATABUFFERTYPE := true
-BOARD_USE_S3D_SUPPORT := true
-BOARD_USE_DMA_BUF := true
-BOARD_USE_ANB_OUTBUF_SHARE := true
-BOARD_USE_GSC_RGB_ENCODER := true
-BOARD_USE_IMPROVED_BUFFER := true
-BOARD_USE_CSC_HW := false
-BOARD_USE_H264_PREPEND_SPS_PPS := false
-BOARD_USE_QOS_CTRL := false
-BOARD_USE_VP8ENC_SUPPORT := true
-BOARD_USE_ENCODER_RGBINPUT_SUPPORT := true
-BOARD_USE_DUALDPB_MODE := true
-
-# Samsung Gralloc
-TARGET_SAMSUNG_GRALLOC_EXTERNAL_USECASES := true
-
-# Partitions
-BOARD_BOOTIMAGE_PARTITION_SIZE := 8388608
-#HAX: real block size is too small to build a ROM
-#BOARD_RECOVERYIMAGE_PARTITION_SIZE := 8388608
-BOARD_RECOVERYIMAGE_PARTITION_SIZE := 10485760
-BOARD_SYSTEMIMAGE_PARTITION_SIZE := 2524971008
-# 12863930368 - 16384 <encryption footer>
-BOARD_USERDATAIMAGE_PARTITION_SIZE := 12863913984
-BOARD_FLASH_BLOCK_SIZE := 4096
-
-# PowerHAL
-TARGET_POWERHAL_VARIANT := universal5420
-
-# Recovery
-COMMON_GLOBAL_CFLAGS += -DNO_SECURE_DISCARD
-BOARD_USE_CUSTOM_RECOVERY_FONT := \"roboto_23x41.h\"
-BOARD_HAS_NO_SELECT_BUTTON := true
-TARGET_RECOVERY_FSTAB := $(LOCAL_PATH)/rootdir/etc/fstab.universal5420
-TARGET_RECOVERY_PIXEL_FORMAT := BGRA_8888
-TARGET_USERIMAGES_USE_EXT4 := true
-BOARD_RECOVERY_SWIPE := true
-RECOVERY_GRAPHICS_USE_LINELENGTH := true
-
-# SELinux
-BOARD_SEPOLICY_DIRS += \
-    device/samsung/lt03wifi/sepolicy
-
-BOARD_SEPOLICY_UNION += \
-    file_contexts \
-    device.te \
-    domain.te \
-    drmserver.te \
-    file.te \
-    gpsd.te \
-    macloader.te \
-    mediaserver.te \
-    service_contexts \
-    servicemanager.te \
-    system_app.te \
-    system_server.te \
-    vold.te \
-    wpa.te
-
-# Webkit
-ENABLE_WEBGL := true
-
-# WFD
-BOARD_USES_WFD_SERVICE := true
-BOARD_USES_WFD := true
-
-# Wifi
-BOARD_HAVE_SAMSUNG_WIFI          := true
-BOARD_WLAN_DEVICE                := bcmdhd
-WPA_SUPPLICANT_VERSION           := VER_0_8_X
-BOARD_WPA_SUPPLICANT_DRIVER      := NL80211
-BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_bcmdhd
-BOARD_HOSTAPD_DRIVER             := NL80211
-BOARD_HOSTAPD_PRIVATE_LIB        := lib_driver_cmd_bcmdhd
-WIFI_DRIVER_FW_PATH_PARAM        := "/sys/module/dhd/parameters/firmware_path"
-WIFI_DRIVER_FW_PATH_STA          := "/system/etc/wifi/bcmdhd_sta.bin"
-WIFI_DRIVER_FW_PATH_AP           := "/system/etc/wifi/bcmdhd_apsta.bin"
-
-# Keepin it block based for the sake of SuperSu
-BLOCK_BASED_OTA=true
 
 # inherit from the proprietary version
 -include vendor/samsung/lt03wifi/BoardConfigVendor.mk
